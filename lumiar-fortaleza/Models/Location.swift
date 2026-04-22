21484fcade659ebcbd5f7926d17361fe3df759d5
marcelodantasf <mdantasfilho2005@gmail.com>
2026-04-22 10:24:54 -0300

    feat: defined Models attributes

--- lumiar-fortaleza/Models/Location.swift
+++ lumiar-fortaleza/Models/Location.swift
@@ -5,3 +5,14 @@
 //  Created by User on 22/04/26.
 //
 
+import Foundation
+
+struct Location: Identifiable {
+    let id              = UUID()
+    let name            : String
+    let Title           : String
+    let Address         : String
+    let Description     : String
+    let Category        : LocationType
+    let Neighbourhood   : Neighbourhood
+}
--- lumiar-fortaleza/Models/Neighbourhood.swift
+++ lumiar-fortaleza/Models/Neighbourhood.swift
@@ -5,3 +5,9 @@
 //  Created by User on 22/04/26.
 //
 
+import Foundation
+
+struct Neighbourhood: Identifiable {
+    var id = UUID()
+    var name: String
+}
