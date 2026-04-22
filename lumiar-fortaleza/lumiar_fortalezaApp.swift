3d7b85f72158971e775d9d7d474f3db5afc48df9
taiunara <anatainarabat@gmail.com>
2026-04-22 08:52:42 -0300

    First Commit

---
+++ lumiar-fortaleza/lumiar_fortalezaApp.swift
@@ -0,0 +1,17 @@
+//
+//  lumiar_fortalezaApp.swift
+//  lumiar-fortaleza
+//
+//  Created by User on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct lumiar_fortalezaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
